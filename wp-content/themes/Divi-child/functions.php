1bcdbb65bad04578dfcb3a6096d13e5b74fb120e
Webmaster <nlscwebmaster11@gmail.com>
2016-12-01 23:43:53 +0000

    added login failure redirect code to Child theme functions.php

--- wp-content/themes/Divi-child/functions.php
+++ wp-content/themes/Divi-child/functions.php
@@ -2,4 +2,41 @@
 function admin_default_page() {
   return '/members-home-page';
 }
+/**
+ * Function Name: front_end_login_fail.
+ * Description: This redirects the failed login to the custom login page instead of default login page with a modified url
+**/
+add_action( 'wp_login_failed', 'front_end_login_fail' );
+function front_end_login_fail( $username ) {
+
+// Getting URL of the login page
+$referrer = $_SERVER['HTTP_REFERER'];    
+// if there's a valid referrer, and it's not the default log-in screen
+if( !empty( $referrer ) && !strstr( $referrer,'wp-login' ) && !strstr( $referrer,'wp-admin' ) ) {
+    wp_redirect( get_permalink( 430 ) . "?login=failed" ); 
+    exit;
+}
+
+}
+
+/**
+ * Function Name: check_username_password.
+ * Description: This redirects to the custom login page if user name or password is   empty with a modified url
+**/
+add_action( 'authenticate', 'check_username_password', 1, 3);
+function check_username_password( $login, $username, $password ) {
+
+// Getting URL of the login page
+$referrer = $_SERVER['HTTP_REFERER'];
+
+// if there's a valid referrer, and it's not the default log-in screen
+if( !empty( $referrer ) && !strstr( $referrer,'wp-login' ) && !strstr( $referrer,'wp-admin' ) ) { 
+    if( $username == "" || $password == "" ){
+        wp_redirect( get_permalink( 430 ) . "?login=empty" );
+        exit;
+    }
+}
+
+}
+// Replace my constant 'LOGIN_PAGE_ID' with your custom login page id.
 ?>
