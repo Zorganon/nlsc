39f015cd7a315fa0197bd3b4023bb9d8d73340f9
Webmaster <nlscwebmaster11@gmail.com>
2016-12-08 23:14:16 +0000

    added ithemes security

--- wp-config.php
+++ wp-config.php
@@ -1,4 +1,10 @@
 <?php
+
+// BEGIN iThemes Security - Do not modify or remove this line
+// iThemes Security Config Details: 2
+define( 'DISALLOW_FILE_EDIT', true ); // Disable File Editor - Security > Settings > WordPress Tweaks > File Editor
+// END iThemes Security - Do not modify or remove this line
+
 if (isset($_SERVER["HTTP_X_FORWARDED_PROTO"]) && $_SERVER["HTTP_X_FORWARDED_PROTO"] == "https") $_SERVER["HTTPS"] = "on";
 /**
  * The base configuration for WordPress
